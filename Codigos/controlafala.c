d6c8eddabbfbb1955e9948c76aeeee3294d2f1d9
thassio23 <thassio.96@gmail.com>
2018-10-29 20:33:56 -0300

    Update controlafala.c

--- Codigos/controlafala.c
+++ Codigos/controlafala.c
@@ -17,13 +17,53 @@ void SetaFlag(){
 flag = 0;
 }
 
+const char * LerVoz()
+{
+	FILE *f; 
+	char text[130];
+	static char  trat[8], cmp[130];
+	
+	strcpy(text,"semorde");
+	strcpy(trat,"semorde");
+	
+	
+	f = fopen("/var/log/syslog","r");		//ABRE O SYSLOG
+	fseek(f, -130, SEEK_END);				//COLOCA O CURSOR DE LEITURA NO FINAL DO ARQUIVO E VOLTA 130 POSIÇÕES
+		
+	fgets(text, 130, f);					//COPIA OS 130 ULTIMOS CARACTERES
+
+	fclose(f);
+
+	if(strcmp(text,cmp) == 0)				//COMPARA PRA N EXCUTAR ORDENS REPETIDAS
+		return "semorde";
+		
+	strcpy(cmp,text);
+	strcpy(trat, &text[122]);				//PEGA SÓ A PARTE DA ORDEM DO STRING
+	
+
+	
+	if(strcmp(trat,"proximo") == 0)			//RETORNA APENAS A INFORMAÇÃO DESEJADA
+		return trat;	
+	if(strcmp(trat,"voltaaa") == 0)
+		return trat;
+	if(strcmp(trat,"repetee") == 0)
+		return trat;
+	if(strcmp(trat,"ler1234") == 0)
+		return trat;
+	if(strcmp(trat,"para123") == 0)
+		return trat;
+	else
+		return "semorde";					//IGNORA POSSIVEIS MENSAGENS NO SYSLOG
+
+}
+
 
 int main(void)
 {
 
         int     fd[2], nbytes;
         int   	pid;
-        char    string[20];
+        char    string[20], para[20];
         char    readbuffer[80];
         char	comando[12];
 
@@ -38,55 +78,92 @@ int main(void)
 
         if(pid == 0)
         {		
-
-        		while(1){
+        		while(1)
+        		{
         		flagchild = 0;
                 close(fd[1]);
-                nbytes = read(fd[0], readbuffer, sizeof(readbuffer));  
-                strcpy(comando, "./fala ");
-                strcat(comando, readbuffer);
-                system(comando);  
+                nbytes = read(fd[0], readbuffer, sizeof(readbuffer)); 
+                
+                if (strcmp(readbuffer,"90") == 0)
+					exit(0);
+                
+                   strcpy(comando, "mpg123 ");
+  		   strcat(comando, readbuffer);
+   		   strcat(comando, ".mp3");
+                   system(comando); 					//AKI ELA FALA
 
-                if(flagchild != 1){       
-                kill(pid, SIGUSR1);    
-                }   
+                if(flagchild != 1)     
+					kill(pid, SIGUSR1);    
+ 
 
                 }
+              
 
         }
         else	
         {	
-        		signal(SIGINT, SIG_IGN); 
+				signal(SIGINT, SIG_IGN); 
                 close(fd[0]);
                 int 	toca = 0;
         		char 	tocastring[3];
         		char mensagem[10];
-    			while(1){
-    			scanf("%s", string);
+    			while(1)
+    			{
+					while(1)
+					{
+						strcpy(string,"semorde");
+						strcpy(string,LerVoz());				// GUARDA A ORDEM EM UM VETOR DENOTRO DA MAIN
+						strcpy(para, string);
+						sleep(1);
+						if(strcmp(string, "proximo") == 0 || strcmp(string, "repetee") == 0 || strcmp(string, "ler1234") == 0 || strcmp(string, "para123") == 0 || strcmp(string, "voltaaa") == 0)
+						break;
 
-                if(strcmp(string, "leia") == 0 || strcmp(string, "repete") == 0 ){	
-                toca = toca;
-                }
-
-                if(strcmp(string, "proximo") == 0){	
-                toca++;
-                }
-
-                if(strcmp(string, "volta") == 0 ){	
-                if(toca  == 0){
-                toca = toca;
-                }else{
-                toca--;
-            	}
-                }
-
+					}
+					
+				if(strcmp(string,"semorde") != 0)
+				{
+					
+					
+					if(strcmp(string, "ler1234") == 0 || strcmp(string, "repetee") == 0 ){	
+						toca = toca;
+						strcpy(string,"x");
+					}
+					if(strcmp(string, "proximo") == 0){
+						toca++;
+						strcpy(string,"x");
+					}
+					if(strcmp(string, "para123") == 0){
+						toca = 90;
+						strcpy(string,"x");
+					}
+					if(strcmp(string, "voltaaa") == 0 )
+					{	
+						if(toca  == 0){
+							toca = toca;
+							strcpy(string,"x");
+						}else{
+							toca--;
+							strcpy(string,"x");
+							}
+					}
+					strcpy(string,"semorde");
+				}
                 sprintf(tocastring, "%d", toca);
-    			if(flag == 1){	
-                kill(0,SIGUSR2);
-                }
+    			if(flag == 1)
+					kill(0,SIGUSR2);
                 flag = 1;
-                write(fd[1], tocastring, (strlen(tocastring)+1));
-                
+                if(strcmp(string,"semorde") != 1)
+					write(fd[1], tocastring, (strlen(tocastring)+1));
+				
+				
+				if(strcmp(para,"para123") == 0)
+				{
+					sleep(1);
+					exit(0);
+				}	
+                strcpy(string,"semorde");
+               
+                sleep(20);
       			}
 
 
